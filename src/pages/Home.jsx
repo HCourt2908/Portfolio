b46e2683f51aa973d2b5dc3ed06e313a050379c2
Henry <wr23643@bristol.ac.uk>
2026-08-01 16:20:42 +0100

    changed title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
